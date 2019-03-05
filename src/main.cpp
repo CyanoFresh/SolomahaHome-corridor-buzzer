8a010e2543ad019e1db77f5f3e1b57be35ef74af
Alex Solomaha <cyanofresh@gmail.com>
2019-03-05 23:55:26 +0200

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,8 @@
 AsyncMqttClient mqttClient;
 Ticker mqttReconnectTimer;
 
+WiFiEventHandler wifiConnectHandler;
+WiFiEventHandler wifiDisconnectHandler;
 Ticker wifiReconnectTimer;
 
 Ticker unlockTimer;
@@ -29,125 +31,124 @@ bool isRinging = false;
 bool isUnlocking = false;
 
 void unlock(const int stage) {
-  if (stage == 0) {
-    Serial.println("Unlock called. Answering...");
+    if (stage == 0) {
+        Serial.println("Unlock called. Answering...");
 
-    isUnlocking = true;
-    
-    digitalWrite(answerPin, 1);
+        isUnlocking = true;
 
-    unlockTimer.once(UNLOCK_DELAY, unlock, 1);
-  } else if (stage == 1) {
-    Serial.println("Pressing unlock button...");
+        digitalWrite(answerPin, 1);
 
-    digitalWrite(unlockPin, 1);
+        unlockTimer.once(UNLOCK_DELAY, unlock, 1);
+    } else if (stage == 1) {
+        Serial.println("Pressing unlock button...");
 
-    unlockTimer.once(UNLOCKING_DURATION, unlock, 2);
-  } else if (stage == 2) {
-    Serial.println("Unlocked. Hanging up...");
+        digitalWrite(unlockPin, 1);
 
-    isUnlocking = false;
+        unlockTimer.once(UNLOCKING_DURATION, unlock, 2);
+    } else if (stage == 2) {
+        Serial.println("Unlocked. Hanging up...");
 
-    digitalWrite(answerPin, 0);
-    digitalWrite(unlockPin, 0);
+        isUnlocking = false;
 
-    mqttClient.publish("buzzer/corridor-buzzer/unlocked", 0, false, "true");
-  }
+        digitalWrite(answerPin, 0);
+        digitalWrite(unlockPin, 0);
+
+        mqttClient.publish("buzzer/corridor-buzzer/unlocked", 0, false, "true");
+    }
 }
 
 void ringingCheck() {
-  if (isUnlocking) {
-    return;
-  }
-
-  Serial.println(analogRead(A0)); // debug
-
-  if (analogRead(A0) > 512) {
-    if (!isRinging) {
-      Serial.println("Call detected");
-
-      isRinging = true;
-
-      mqttClient.publish("buzzer/corridor-buzzer/ringing", 0, false, "true");
+    if (isUnlocking) {
+        return;
     }
-  } else {
-    if (isRinging) {
-      Serial.println("Call end detected");
 
-      isRinging = false;
+//    Serial.println(analogRead(A0)); // debug
 
-      mqttClient.publish("buzzer/corridor-buzzer/ringing", 0, false, "false");
+    if (analogRead(A0) > 512) {
+        if (!isRinging) {
+            Serial.println("Call detected");
+
+            isRinging = true;
+
+            mqttClient.publish("buzzer/corridor-buzzer/ringing", 0, false, "true");
+        }
+    } else {
+        if (isRinging) {
+            Serial.println("Call end detected");
+
+            isRinging = false;
+
+            mqttClient.publish("buzzer/corridor-buzzer/ringing", 0, false, "false");
+        }
     }
-  }
 }
 
 void connectToWifi() {
-  Serial.println("Connecting to Wi-Fi...");
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.println("Connecting to Wi-Fi...");
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
 }
 
 void connectToMqtt() {
-  Serial.println("Connecting to MQTT...");
-  mqttClient.connect();
+    Serial.println("Connecting to MQTT...");
+    mqttClient.connect();
 }
 
-void onWifiConnect(const WiFiEventStationModeGotIP& event) {
-  Serial.println("Connected to Wi-Fi.");
-  connectToMqtt();
+void onWifiConnect(const WiFiEventStationModeGotIP &event) {
+    Serial.println("Connected to Wi-Fi.");
+    connectToMqtt();
 }
 
-void onWifiDisconnect(const WiFiEventStationModeDisconnected& event) {
-  Serial.println("Disconnected from Wi-Fi.");
-  mqttReconnectTimer.detach();
-  wifiReconnectTimer.once(2, connectToWifi);
+void onWifiDisconnect(const WiFiEventStationModeDisconnected &event) {
+    Serial.println("Disconnected from Wi-Fi.");
+    mqttReconnectTimer.detach();
+    wifiReconnectTimer.once(2, connectToWifi);
 }
 
 void onMqttConnect(bool) {
-  Serial.println("Connected to MQTT.");
+    Serial.println("Connected to MQTT.");
 
-  // Subscribe
-  mqttClient.subscribe("buzzer/corridor-buzzer/unlock", 0);
-  mqttClient.subscribe("devices/corridor-buzzer", 0);
+    // Subscribe to topics:
+    mqttClient.subscribe("buzzer/corridor-buzzer/unlock", 0);
 }
 
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
-  Serial.print("Disconnected from MQTT. Reason: ");
-  Serial.println((int)reason);
+    Serial.print("Disconnected from MQTT. Reason: ");
+    Serial.println((int) reason);
 
-  if (WiFi.isConnected()) {
-    mqttReconnectTimer.once(2, connectToMqtt);
-  }
+    if (WiFi.isConnected()) {
+        mqttReconnectTimer.once(2, connectToMqtt);
+    }
 }
 
-void onMqttMessage(char* topic, char* payload,
+void onMqttMessage(char *topic, char *payload,
                    AsyncMqttClientMessageProperties properties, size_t len,
                    size_t index, size_t total) {
-  if (strcmp(topic, "buzzer/corridor-buzzer/unlock") != 0) {
-    unlock(0);
-  }
+    if (strcmp(topic, "buzzer/corridor-buzzer/unlock") == 0) {
+        unlock(0);
+    }
 }
 
 void setup() {
-  pinMode(answerPin, INPUT);
-  pinMode(unlockPin, INPUT);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println();
 
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println();
+    wifiConnectHandler = WiFi.onStationModeGotIP(onWifiConnect);
+    wifiDisconnectHandler = WiFi.onStationModeDisconnected(onWifiDisconnect);
 
-  WiFi.onStationModeGotIP(onWifiConnect);
-  WiFi.onStationModeDisconnected(onWifiDisconnect);
+    pinMode(answerPin, INPUT);
+    pinMode(unlockPin, INPUT);
 
-  mqttClient.onConnect(onMqttConnect);
-  mqttClient.onDisconnect(onMqttDisconnect);
-  mqttClient.onMessage(onMqttMessage);
-  mqttClient.setServer(MQTT_HOST, MQTT_PORT);
-  mqttClient.setClientId(MQTT_ID);
-  mqttClient.setCredentials("device", MQTT_PASSWORD);
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
+    mqttClient.onMessage(onMqttMessage);
+    mqttClient.setServer(MQTT_HOST, MQTT_PORT);
+    mqttClient.setClientId(MQTT_ID);
+    mqttClient.setCredentials("device", MQTT_PASSWORD);
 
-  ringingTimer.attach_ms(100, ringingCheck);
+    ringingTimer.attach_ms(100, ringingCheck);
 
-  connectToWifi();
+    connectToWifi();
 }
 
 void loop() {}
